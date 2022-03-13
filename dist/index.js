26d43ad5bcd24ec2560f4e2b3e6bb69122a740bd
Leandrosiq <le_kts12@hotmail.com>
2022-03-13 10:47:03 -0300

    generate dist updated

--- dist/index.js
+++ dist/index.js
@@ -14,4 +14,4 @@ app.use(_cors2.default.call(void 0, ));
 app.use(_routes.router);
 
 const port = process.env.PORT || 2001;
-app.listen(2001, () => console.log(`Server started on port ${port} 🚀`));
+app.listen(port, () => console.log(`Server started on port ${port} 🚀`));
